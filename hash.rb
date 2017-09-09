7e43b258d0c1ce94d3a9b271816e0725ea31563d
captain <captain@localhost.localdomain>
2017-09-09 17:26:18 +0300

    add hash

---
+++ hash.rb
@@ -0,0 +1,16 @@
+dict= {"name" =>"rachel", "school" =>"KU","age" =>85}
+
+#accessing elements in a hash
+p dict["name"]
+
+p dict["age"]
+
+#othermethods
+
+p dict.keys
+
+p dict.values
+
+p dict.length
+
+
