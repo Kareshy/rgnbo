87d4e89529be0b3982776ded724bc676ae1dd873
captain <captain@localhost.localdomain>
2017-09-09 16:44:43 +0300

    ruby strings

---
+++ hello.rb
@@ -0,0 +1,28 @@
+#single line comment
+#print hello world
+
+puts "hello world"
+
+
+=begin
+	multiline comment
+	this code
+	another comment
+=end
+
+#Datatypes
+	#string
+puts "am a double"
+#interpolation
+
+age = 20
+
+puts "my age is #{age}"
+
+puts 'single quoted'
+
+#string method
+puts 'my age is #{age}'
+
+puts "firstname"+ " secondname"
+ 
